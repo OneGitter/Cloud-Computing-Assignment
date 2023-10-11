da9dc35f3d6896d0eb9ce4a9d3ec329f078e757e
OneGitter <mnu202001@gmail.com>
2023-10-11 15:27:36 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>My React Page</h1>
+        <p>Name: Mohammed Naseer Uddin</p>
+        <p>Enrollment Number: 20UCS112</p>
+        <p>Registration Number: 2012702</p>
+        <p>College: National Institute of Technology Agartala</p>
+        <p>Cloud Computing Assignment: React App</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
